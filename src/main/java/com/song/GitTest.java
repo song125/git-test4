87b068abc99590cb9d4f59e75bd51b6c9e2d4782
song <song@163.com>
2022-12-18 10:17:22 +0800

    first commit

---
+++ src/main/java/com/song/GitTest.java
@@ -0,0 +1,11 @@
+package com.song;
+
+/**
+ * @author :song
+ * @create :2022 12月 18 10:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
