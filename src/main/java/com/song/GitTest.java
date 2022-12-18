12314a330984166ce69d0509a5970904596afbc7
song <song@163.com>
2022-12-18 10:21:57 +0800

    second commit

--- src/main/java/com/song/GitTest.java
+++ src/main/java/com/song/GitTest.java
@@ -7,5 +7,6 @@ package com.song;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
